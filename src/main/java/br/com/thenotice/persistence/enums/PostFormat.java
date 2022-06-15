aba4d1a48d414e5efd52873a882f22cd3512990d
kleverson Holanda <kleverson.holanda@gmail.com>
2022-06-14 21:07:22 -0300

    add post format, post type, initial implementation for upload file

---
+++ src/main/java/br/com/thenotice/persistence/enums/PostFormat.java
@@ -0,0 +1,2 @@
+package br.com.thenotice.persistence.enums;public class PostFormat {
+}
---
+++ src/main/java/br/com/thenotice/util/StorageService.java
@@ -0,0 +1,11 @@
+package br.com.thenotice.util;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
+
+@Service
+public class StorageService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(StorageService.class);
+}
